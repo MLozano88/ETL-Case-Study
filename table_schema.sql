3baa662d1f43082dd390bbe68bbb07bc6004c80e
Ronjini Hassan <ronjinihassan@ronjinis-mbp.home>
2021-02-13 14:20:40 -0500

    fixed schema

--- table_schema.sql
+++ table_schema.sql
@@ -1,40 +1,24 @@
-DROP TABLE IF EXISTS tmdb;
-DROP TABLE IF EXISTS netflix;
-DROP TABLE IF EXISTS oscars;
+DROP TABLE IF EXISTS tmdb CASCADE;
+DROP TABLE IF EXISTS netflix CASCADE;
+DROP TABLE IF EXISTS oscars CASCADE;
 
-CREATE TABLE "tmdb" (
-    "ID" INT   NOT NULL,
-    "Movie" VARCHAR   NOT NULL,
-    "Description" VARCHAR   NOT NULL,
-    "Vote_Count" INT   NOT NULL,
-    "Vote_Average" FLOAT   NOT NULL,
-    CONSTRAINT "pk_tmdb" PRIMARY KEY (
-        "Movie"
-     )
+CREATE TABLE "tmdb"(    
+	"Id" INT NOT NULL,
+    "Movie" VARCHAR NOT NULL,
+    "Description" VARCHAR NOT NULL,
+    "Vote_Count" INT NOT NULL,
+    "Vote_Average" FLOAT NOT NULL
 );
 
 CREATE TABLE "netflix" (
     "Movie" VARCHAR   NOT NULL,
     "Director" VARCHAR   NOT NULL,
     "Country" VARCHAR   NOT NULL,
-    "Year" INT   NOT NULL,
-    CONSTRAINT "pk_netflix" PRIMARY KEY (
-        "Movie"
-     )
+    "Year" INT   NOT NULL
 );
 
 CREATE TABLE "oscars" (
     "Movie" VARCHAR   NOT NULL,
     "Award" VARCHAR   NOT NULL,
-    "Year_of_Award" INT   NOT NULL,
-    CONSTRAINT "pk_oscars" PRIMARY KEY (
-        "Movie"
-     )
+    "Year_of_Award" INT   NOT NULL
 );
-
-ALTER TABLE "netflix" ADD CONSTRAINT "fk_netflix_Movie" FOREIGN KEY("Movie")
-REFERENCES "tmdb" ("Movie");
-
-ALTER TABLE "oscars" ADD CONSTRAINT "fk_oscars_Movie" FOREIGN KEY("Movie")
-REFERENCES "tmdb" ("Movie");
-
