12640398b37828522f14393edb1df5ffe91acf76
Mario Lozano <Mario@m.lozano1733.com>
2021-02-16 20:27:33 -0500

    updates

--- queries.sql
+++ queries.sql
@@ -1,8 +1,9 @@
 -- Merge Netflix and TMDB tables on movie name and rank the movies by vote average in descending order and only show results that have 500 or more votes
-This will show us the netflix mocies 
+This will show us the netflix movies 
 SELECT DISTINCT tmdb."Movie", tmdb."Vote_Count", tmdb."Vote_Average"
 FROM tmdb
 INNER JOIN netflix ON
 netflix."Movie" = tmdb."Movie"
+WHERE tmdb."Vote_Count" >= 1000
 ORDER BY tmdb."Vote_Average" desc;
 
